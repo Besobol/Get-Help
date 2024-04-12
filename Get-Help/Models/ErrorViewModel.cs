a476543823310afe098c46efc1ad7231b3a490ff
Dragomir Kamberov <dkamberov@hotmail.com>
2024-04-12 19:35:20 +0300

    Add project files.

---
+++ Get-Help/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Get_Help.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
