1453b69c4aa01ec103a020059f51cdae6e98adb8
Dragomir Kamberov <dkamberov@hotmail.com>
2024-04-13 19:05:23 +0300

    Added Identity.

--- Get-Help/Extensions/ServiceCollectionExtensions.cs
+++ Get-Help/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Get_Help_Infrastructure.Data;
+using Get_Help_Infrastructure.Data.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Microsoft.Extensions.DependencyInjection
@@ -9,6 +10,25 @@ namespace Microsoft.Extensions.DependencyInjection
         {
             string connectionString = config.GetConnectionString("DefaultConnection");
             services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationIdentity(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDefaultIdentity<ApplicationUser>(options =>
+            {
+                options.User.RequireUniqueEmail = true;
+                options.SignIn.RequireConfirmedEmail = false;
+                options.Lockout.AllowedForNewUsers = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 8;
+            })
+            .AddRoles<ApplicationRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>();
 
             return services;
         }
--- Get-Help/Program.cs
+++ Get-Help/Program.cs
@@ -1,9 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApplicationDbContext(builder.Configuration);
+builder.Services.AddApplicationIdentity(builder.Configuration);
 
 builder.Services.AddControllersWithViews();
-
 
 var app = builder.Build();
 
