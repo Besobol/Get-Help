4ae96f816585d6b33c9eee01e599e9d550c94856
Dragomir Kamberov <dkamberov@hotmail.com>
2024-04-13 23:05:11 +0300

    Added basic repository

---
+++ Get-Help-Infrastructure/Data/Common/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Get_Help_Infrastructure.Data.Common
+{
+    public interface IRepository
+    {
+        IQueryable<T> All<T>() where T : class;
+
+        IQueryable<T> AllReadOnly<T>() where T : class;
+
+        Task<int> SaveChangesAsync();
+    }
+}
