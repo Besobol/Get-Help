7d140704aed2aa39b72a2bd72a58d837b926611b
Dragomir Kamberov <dkamberov@hotmail.com>
2024-04-24 01:42:47 +0300

    Updated Repository

--- Get-Help-Infrastructure/Data/Common/IRepository.cs
+++ Get-Help-Infrastructure/Data/Common/IRepository.cs
@@ -6,6 +6,12 @@
 
         IQueryable<T> AllReadOnly<T>() where T : class;
 
+        Task AddAsync<T>(T entity) where T : class;
+
+        Task<T?> GetByIdAsync<T>(object id) where T : class;
+
+        Task DeleteAsync<T>(object id) where T : class;
+
         Task<int> SaveChangesAsync();
     }
 }
--- Get-Help-Infrastructure/Data/Common/Repository.cs
+++ Get-Help-Infrastructure/Data/Common/Repository.cs
@@ -27,6 +27,26 @@ namespace Get_Help.Infrastructure.Data.Common
             return DbSet<T>().AsNoTracking();
         }
 
+        public async Task AddAsync<T>(T entity) where T : class
+        {
+            await DbSet<T>().AddAsync(entity);
+        }
+
+        public async Task<T?> GetByIdAsync<T>(object id) where T : class
+        {
+            return await DbSet<T>().FindAsync(id);
+        }
+
+        public async Task DeleteAsync<T>(object id) where T : class
+        {
+            T? entity = await GetByIdAsync<T>(id);
+
+            if (entity != null)
+            {
+                DbSet<T>().Remove(entity);
+            }
+        }
+
         public async Task<int> SaveChangesAsync()
         {
             return await context.SaveChangesAsync();
